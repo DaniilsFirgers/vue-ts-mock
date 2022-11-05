d44d4905bc8289d5389e9f7f93184ddec8c4ab6c
dfirgers <dfirger@gmail.com>
2022-11-05 22:36:21 +0200

    fix axios requests and add types for it

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,17 +5,97 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
 import axios from "axios";
+
+const weatherData = ref({});
+const requestError = ref({});
 
 interface WeatherForecast {
   cod: String;
+  city: City;
   message: Number;
   cnt: Number;
   list: List[];
 }
+
+interface City {
+  coord: Coord;
+  country: string;
+  id: number;
+  name: String;
+  population: number;
+  sunrise: number;
+  sunset: number;
+  timezone: number;
+}
+
+interface Coord {
+  lat: number;
+  lon: number;
+}
+
+interface Main {
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  pressure: number;
+  sea_level: number;
+  grnd_level: number;
+  humidity: number;
+  temp_kf: number;
+}
 interface List {
   dt: number;
+  main: Main;
+  weather: Weather[];
+  clouds: Clouds;
+  wind: Wind;
+  rain?: Rain;
+  sys: Sys;
+  visibility: number;
+  pop: number;
+}
+
+interface Weather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+interface Clouds {
+  all: number;
+}
+
+interface Wind {
+  speed: number;
+  deg: number;
+  gust: number;
+}
+
+interface Rain {
+  h: string;
+}
+
+interface Sys {
+  pod: string;
+}
+
+async function fetchData() {
+  await axios
+    .get(
+      "https://api.openweathermap.org/data/2.5/forecast?lat=56.946&lon=24.10589&appid=e3f9a6c2b88332ca04725bd27f037716"
+    )
+    .then((response) => {
+      const data: WeatherForecast = response.data;
+      weatherData.value = data;
+      console.log("data", weatherData.value);
+    })
+    .catch((err) => {
+      console.log(err);
+      requestError.value == err;
+    });
 }
 
 export default defineComponent({
@@ -25,16 +105,9 @@ export default defineComponent({
       info: null,
     };
   },
-  methods: {
-    async fetchData() {
-      const response = await axios.get(
-        "https://api.openweathermap.org/data/2.5/forecast?lat=56.946&lon=24.10589&appid=e3f9a6c2b88332ca04725bd27f037716"
-      );
-      console.log(response);
-    },
-  },
-  async mounted() {
-    this.fetchData();
+  methods: {},
+  setup() {
+    fetchData();
   },
 });
 </script>
