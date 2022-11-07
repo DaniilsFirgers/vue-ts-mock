3835598fbf7ae40df9bc99282bd01b967743b67a
dfirgers <dfirger@gmail.com>
2022-11-07 19:54:06 +0200

    add logic to requests and data display

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <div v-show="showElement">Visible only three seconds</div>
+    <div v-show="loadingIconDiv" class="loading-icon"></div>
+    <div v-for="(item, index) in weatherData" :key="index">
+      <h1>{{ item.datetime }}</h1>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, onMounted, ref } from "vue";
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
 import axios from "axios";
 
-const weatherData = ref({});
+let weatherData = ref<
+  {
+    datetime: number;
+    temperature: number;
+    clouds: number;
+    windSpeed: number;
+    humidity: number;
+  }[]
+>([]);
 const requestError = ref({});
 const loadingIconDiv = ref<boolean>(true);
 
@@ -84,42 +95,77 @@ interface Sys {
   pod: string;
 }
 
-async function fetchData() {
+const fetchData = async () => {
   await axios
     .get(
       "https://api.openweathermap.org/data/2.5/forecast?lat=56.946&lon=24.10589&appid=e3f9a6c2b88332ca04725bd27f037716"
     )
     .then((response) => {
       const data: WeatherForecast = response.data;
-      weatherData.value = data;
+      const arrayOfHourlyData = data.list;
+
+      var arrayOfWeatherObjects: any = [];
+
+      arrayOfHourlyData.forEach((el) => {
+        const weatherObject = {
+          datetime: el.dt,
+          temperature: el.main.temp,
+          clouds: el.clouds,
+          windSpeed: el.wind.speed,
+          humidity: el.main.humidity,
+        };
+        arrayOfWeatherObjects.push(weatherObject);
+      });
+      weatherData.value = arrayOfWeatherObjects;
       console.log("data", weatherData.value);
     })
     .catch((err) => {
       console.log(err);
       requestError.value == err;
     });
-}
+};
 
-function loadingIcon() {
+const loadingIcon = () => {
   setTimeout(() => {
     loadingIconDiv.value = false;
-    console.log("aaaS");
   }, 3000);
-  console.log(loadingIconDiv.value);
-}
+};
 
-export default defineComponent({
-  name: "WeatherData",
-  data() {
-    return { showElement: loadingIconDiv.value };
-  },
-  methods: {},
-  setup() {
-    fetchData();
-    loadingIcon();
-  },
-  mount() {},
+// export default defineComponent({
+//   name: "WeatherData",
+//   data() {
+//     return {
+//       showElement: loadingIconDiv,
+//       data: {} as WeatherForecast,
+//     };
+//   },
+//   methods: {},
+//   setup() {
+//     // loadingIcon();
+//     // fetchData();
+//   },
+// });
+onMounted(() => {
+  loadingIcon();
+  fetchData();
 });
 </script>
 
-<style></style>
+<style>
+.loading-icon {
+  border: 16px solid #f3f3f3;
+  border-top: 16px solid #3498db;
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  animation: spin 2s linear infinite;
+}
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
